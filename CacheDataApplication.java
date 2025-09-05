6dd1175a5f53c5569d58b022c7f49625887e7289
ansar619don-cell <ansar619don@gmail.com>
2025-09-05 10:26:46 +0530

    Added cachedata

---
+++ CacheDataApplication.java
@@ -0,0 +1,13 @@
+package com.ansarsproject.cacheData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CacheDataApplication.class, args);
+	}
+
+}
